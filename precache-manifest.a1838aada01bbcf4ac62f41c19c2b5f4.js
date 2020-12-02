9d2d0cbe77e61617eaa513db405e86201d27a1d4
Jad <jad_mohammad@hotmail.com>
2020-12-02 12:05:32 -0500

    Updates

---
+++ precache-manifest.a1838aada01bbcf4ac62f41c19c2b5f4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59368f770e83a1f8f2ab3c0a4c80b587",
+    "url": "/Jadbaltaji/react-calculaternpm/index.html"
+  },
+  {
+    "revision": "3e0cb739fafee1c1f24d",
+    "url": "/Jadbaltaji/react-calculaternpm/static/css/main.2ecf7b71.chunk.css"
+  },
+  {
+    "revision": "b011ce21a6da794d9886",
+    "url": "/Jadbaltaji/react-calculaternpm/static/js/2.add7bac3.chunk.js"
+  },
+  {
+    "revision": "3e0cb739fafee1c1f24d",
+    "url": "/Jadbaltaji/react-calculaternpm/static/js/main.ffcddbec.chunk.js"
+  },
+  {
+    "revision": "b0b30eb46271aa9a70b4",
+    "url": "/Jadbaltaji/react-calculaternpm/static/js/runtime~main.dbc63843.js"
+  }
+]);
